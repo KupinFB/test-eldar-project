f00858962c6d89b1ed747f67166cef2880470c64
Sergey Kupin <kupin27@gmail.com>
2023-01-23 16:37:33 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const usersService = require('./services/users-service.js');
+
+usersService.add({
+    id: 1,
+    name: '',
+    age: 12
+});
+
+
+// usersService.add({
+//     name: 'Sergey1',
+//     age: 34
+// });
+
+// usersService.remove(1);
+// usersService.update({
+//     id: 2,
+//     name: 'Sergey 2',
+//     age: 36
+// })
+
+// console.log(usersService.list);
+// console.log(usersService.get(2));
+
+
+
+
